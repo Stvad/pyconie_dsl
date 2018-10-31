0a4ede705956b3440238e9a2b1e7d1de283fa4b4
Vladyslav Sitalo <sitalov@amazon.com>
2018-10-31 20:28:18 +0000

    Add dynamic field generation example

---
+++ examples/dynamic_generatioin.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class XmlBuilder:
+    tag_name: str = "html"
+
+    def __enter__(self):
+        print(f'<{self.tag_name}>')
+
+    def __exit__(self, *args):
+        print(f'</{self.tag_name}>')
+
+    def __getattr__(self, item):
+        return XmlBuilder(item)
+
+
+t = XmlBuilder()
+
+with t.body:
+    with t.h1:
+        print("Whee")
