8f43bd5d375e0833173cf6288ee92ef9afa7338b
Vladyslav Sitalo <sitalov@amazon.com>
2018-11-01 22:34:52 +0000

    AsciiCanvas clean up + add nicer example

--- examples/magic_overloading.py
+++ examples/magic_overloading.py
@@ -1,7 +1,6 @@
-from copy import copy
 from dataclasses import dataclass, field
 from functools import reduce
-from typing import Dict, Callable
+from typing import Dict, Callable, List
 
 
 @dataclass(frozen=True)
@@ -24,28 +23,32 @@ def coordinate_max(p1, p2):
 
 @dataclass
 class AsciiCanvas:
-    position: Point = Point()
     symbol: str = '*'
     filler: str = ' '
+    position: Point = Point()
+    contextual_symbol: str = '*'
+
     content: Dict[Point, str] = field(default_factory=dict)
+    symbol_stack: List[str] = field(default_factory=list)
 
-    def transform(self, amount, transformation: Callable[[Point, int], Point]):
-        for i in range(1, amount + 1):
-            self.content[transformation(self.position, i)] = self.symbol
+    def transform(self, amount, transformation: Callable[[Point], Point]):
+        for _ in range(int(amount)):
+            self.content[self.position] = self.symbol
+            self.position = transformation(self.position)
 
-        self.position = transformation(self.position, amount)
+        self.content[self.position] = self.symbol
 
     def __lt__(self, other):
-        self.transform(other, lambda point, amount: Point(point.x - amount, point.y))
+        self.transform(other, lambda point: Point(point.x - 1, point.y))
 
     def __gt__(self, other):
-        self.transform(other, lambda point, amount: Point(point.x + amount, point.y))
+        self.transform(other, lambda point: Point(point.x + 1, point.y))
 
     def __xor__(self, other):
-        self.transform(other, lambda point, amount: Point(point.x, point.y + amount))
+        self.transform(other, lambda point: Point(point.x, point.y + 1))
 
     def __floordiv__(self, other):
-        self.transform(other, lambda point, amount: Point(point.x, point.y - amount))
+        self.transform(other, lambda point: Point(point.x, point.y - 1))
 
     def __repr__(self):
         lower_left, higher_right = self.get_display_rectangle()
@@ -65,25 +68,61 @@ class AsciiCanvas:
         return reduce(coordinate_min, self.content.keys()), \
                reduce(coordinate_max, self.content.keys())
 
-    # def __call__(self, contextual_symbol):
-    #     return AsciiCanvas(self.position, contextual_symbol, self.filler, self.content)
-    #
-    #
-    # def __enter__(self):
-    #     self.symbol = self.contextual_symbol
+    def __call__(self, contextual_symbol):
+        self.contextual_symbol = contextual_symbol
+        return self
+
+    def __enter__(self):
+        self.symbol_stack.append(self.symbol)
+        self.symbol = self.contextual_symbol
+
+    def __exit__(self, *args):
+        self.symbol = self.symbol_stack.pop()
 
 
-p = AsciiCanvas()
+p = AsciiCanvas('🙂', '  ')
 
 letter_height = 21
-letter_width = letter_height  # makes sense because of the distance diff
+letter_width = letter_height / 2
 
-p // letter_height
-p ^ (letter_height // 2)
 
-p > letter_width
+def space():
+    p > 1
+    with p('  '):
+        p > letter_width / 2
 
-p ^ (letter_height // 2)
-p // letter_height
+
+def reset_height():
+    with p(' '):
+        p ^ letter_height
+
+
+def h():
+    p // letter_height
+    p ^ (letter_height / 2)
+
+    with p('🦄'):
+        p > letter_width
+
+    p // (letter_height / 2)
+    p ^ letter_height
+
+
+def i():
+    p > letter_width / 2
+    p < letter_width / 4
+
+    with p('😛'):
+        p // letter_height
+
+    p < letter_width / 4
+    p > letter_width / 2 + 1
+
+    reset_height()
+
+
+h()
+space()
+i()
 
 print(p)
