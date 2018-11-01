e6997cd2baf44573ff643c937021acf8eb9cd8a0
Vladyslav Sitalo <sitalov@amazon.com>
2018-11-01 21:22:25 +0000

    AsciiCanvas working version

--- examples/magic_overloading.py
+++ examples/magic_overloading.py
@@ -1,8 +1,7 @@
+from copy import copy
 from dataclasses import dataclass, field
 from functools import reduce
 from typing import Dict, Callable
-
-from functional import seq
 
 
 @dataclass(frozen=True)
@@ -11,20 +10,20 @@ class Point:
     y: int = 0
 
 
-def apply_function_on_coordinates(p1, p2, fun):  # todo names
+def apply_function_on_coordinates(p1, p2, fun):
     return Point(fun(p1.x, p2.x), fun(p1.y, p2.y))
 
 
-def coord_min(p1, p2):  # todo names
+def coordinate_min(p1, p2):
     return apply_function_on_coordinates(p1, p2, min)
 
 
-def coord_max(p1, p2):  # todo names
+def coordinate_max(p1, p2):
     return apply_function_on_coordinates(p1, p2, max)
 
 
 @dataclass
-class AsciiPaint:
+class AsciiCanvas:
     position: Point = Point()
     symbol: str = '*'
     filler: str = ' '
@@ -36,30 +35,20 @@ class AsciiPaint:
 
         self.position = transformation(self.position, amount)
 
-    def __lt__(self, other: int):
-        for dec in range(1, other + 1):
-            self.content[Point(self.position.x - dec, self.position.y)] = self.symbol
-
-        self.position = Point(self.position.x - other, self.position.y)
+    def __lt__(self, other):
+        self.transform(other, lambda point, amount: Point(point.x - amount, point.y))
 
     def __gt__(self, other):
-        return self.transform(other, lambda point, amount: Point(point.x + amount, point.y))
+        self.transform(other, lambda point, amount: Point(point.x + amount, point.y))
 
-    def __xor__(self, other: int):
-        for inc in range(1, other + 1):
-            self.content[Point(self.position.x, self.position.y + inc)] = self.symbol
-
-        self.position = Point(self.position.x, self.position.y + other)
+    def __xor__(self, other):
+        self.transform(other, lambda point, amount: Point(point.x, point.y + amount))
 
     def __floordiv__(self, other):
-        pass
+        self.transform(other, lambda point, amount: Point(point.x, point.y - amount))
 
     def __repr__(self):
         lower_left, higher_right = self.get_display_rectangle()
-
-        # seq(zip(range(lower_left.x, higher_right.x + 1)), range(higher_right.y, lower_left.y - 1, -1)) \
-        #     .map(lambda coord: Point(*coord)) \
-        #     .map(lambda point: self.content.get(point, self.filler))
 
         lines = []
         for y in range(higher_right.y, lower_left.y - 1, -1):
@@ -73,14 +62,28 @@ class AsciiPaint:
         return "\n".join(lines)
 
     def get_display_rectangle(self):
-        return reduce(coord_min, self.content.keys()), \
-               reduce(coord_max, self.content.keys())
+        return reduce(coordinate_min, self.content.keys()), \
+               reduce(coordinate_max, self.content.keys())
+
+    # def __call__(self, contextual_symbol):
+    #     return AsciiCanvas(self.position, contextual_symbol, self.filler, self.content)
+    #
+    #
+    # def __enter__(self):
+    #     self.symbol = self.contextual_symbol
 
 
-p = AsciiPaint()
+p = AsciiCanvas()
 
-p < 5
-p ^ 7
-p > 8
-# p // 7
+letter_height = 21
+letter_width = letter_height  # makes sense because of the distance diff
+
+p // letter_height
+p ^ (letter_height // 2)
+
+p > letter_width
+
+p ^ (letter_height // 2)
+p // letter_height
+
 print(p)
